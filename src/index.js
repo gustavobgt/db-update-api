f530b374a416f5081d74280ca7139f6230b2a5cc
gustavobgt <gustavo_bgt97@hotmail.com>
2020-09-25 16:51:56 -0300

    Implementing sale merging logic

--- src/index.js
+++ src/index.js
@@ -17,10 +17,12 @@ const PATH = './src/data';
 // Excel files
 global.EXCEL_SALES = `${PATH}/SALES.xlsx`;
 global.EXCEL_FINANCES = `${PATH}/FINANCES.xls`;
+global.EXCEL_SERVICES = `${PATH}/SERVICES.xls`;
 
 // Json files
 global.JSON_SALES = `${PATH}/json/SALES.json`;
 global.JSON_FINANCES = `${PATH}/json/FINANCES.json`;
+global.JSON_SERVICES = `${PATH}/json/SERVICES.json`;
 
 async function convertExcelToJson(excelFileName, jsonFileName) {
   try {
@@ -96,19 +98,88 @@ console.log('Starting MongoDB Connection...');
             JSON_SALES,
             EXCEL_FINANCES,
             JSON_FINANCES,
+            EXCEL_SERVICES,
+            JSON_SERVICES,
           } = global;
 
           const sales = await convertion(EXCEL_SALES, JSON_SALES);
           const finances = await convertion(EXCEL_FINANCES, JSON_FINANCES);
+          const services = await convertion(EXCEL_SERVICES, JSON_SERVICES);
 
-          console.log(finances);
+          const DB_ARRAY = [];
+
+          sales.forEach((sale) => {
+            const {
+              customer,
+              carModel,
+              proposalNumber,
+              yearMonthDay,
+              salesPrice,
+              cost,
+              bonus,
+              totalCost,
+              parentUser,
+            } = sale;
+            // FINANCE
+            const foundFinance = finances.find((finance) => {
+              return (
+                parseInt(proposalNumber) === parseInt(finance.proposalNumber)
+              );
+            });
+
+            let tabel_FI = '';
+            let otherTabels = '';
+            let zeroRate = '';
+
+            if (foundFinance) {
+              const { nickname } = foundFinance;
+
+              if (nickname === 'DN') {
+                tabel_FI = 'S';
+              } else if (nickname === 'TX0') {
+                zeroRate = 'S';
+              } else if (
+                nickname === 'SEMPRENV' ||
+                nickname === 'GO50' ||
+                nickname === 'GO40'
+              ) {
+                otherTabels = 'S';
+              }
+            }
+
+            // SERVICES
+            // const foundServices = services.filter((service) => {
+            //   return (
+            //     parseInt(proposalNumber) === parseInt(service.proposalNumber)
+            //   );
+            // });
+
+            const fullSale = {
+              customer,
+              carModel,
+              proposalNumber,
+              yearMonthDay,
+              salesPrice,
+              cost,
+              bonus,
+              totalCost,
+              sellingExpenses,
+              accessories,
+              tabel_FI,
+              otherTabels,
+              zeroRate,
+              parentUser,
+            };
+
+            console.log(fullSale);
+          });
 
           // updateDataBase(sales);
         } catch (err) {
           console.log(err);
         }
       })();
-    }, 5000);
+    }, 2000);
   } catch (err) {
     console.log(`Error Connecting MongoDB - ${err}`);
   }
